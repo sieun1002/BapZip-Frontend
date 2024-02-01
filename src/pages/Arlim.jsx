ffb33dc325ef053f107221fa2f0a9bdce4d1a80a
manto123123123 <lhw971103@gmail.com>
2024-02-02 03:05:20 +0900

    알림컴포넌트추가

--- src/pages/Arlim.jsx
+++ src/pages/Arlim.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Arlim/Header";
 import Silsigan from "../components/Arlim/silsigan";
+import Update from "../components/Arlim/Update";
 
 export default function WriteReview() {
   return (
-    <div className="container-writeReview">
+    <div className="container-BottomNav2">
       <Header />
       <Silsigan />
+      <Update />
     </div>
   );
 }
--- src/pages/WriteReview.jsx
+++ src/pages/WriteReview.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 import Header from "../components/WriteReview/Header";
-import SearchStore from '../components/WriteReview/SearchStore';
-import StarRating from '../components/WriteReview/StarRating';
-import SelectBenefit from '../components/WriteReview/SelectGood';
-import WriteTxt from '../components/WriteReview/WriteTxt';
-import UploadImg from '../components/WriteReview/UploadImg';
+import SearchStore from "../components/WriteReview/SearchStore";
+import StarRating from "../components/WriteReview/StarRating";
+import SelectBenefit from "../components/WriteReview/SelectGood";
+import WriteTxt from "../components/WriteReview/WriteTxt";
+import UploadImg from "../components/WriteReview/UploadImg";
 
 export default function WriteReview() {
   return (
-    <div className='container-writeReview'>
-        <Header />
-        <SearchStore />
-        <StarRating />
-        <SelectBenefit />
-        <WriteTxt />
-        <UploadImg />
+    <div className="container-writeReview">
+      <Header />
+      <SearchStore />
+      <StarRating />
+      <SelectBenefit />
+      <WriteTxt />
+      <UploadImg />
     </div>
-  )
+  );
 }
