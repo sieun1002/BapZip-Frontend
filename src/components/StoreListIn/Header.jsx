f7b12690d44b39ab91babb808e4433c596be11f0
[WalkingUpright] <[ohige01@naver.com]>
2024-02-12 16:28:33 +0900

    storeListIn 틀 만들기

---
+++ src/components/StoreListIn/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import scrBackBtn from '../../images/WriteReview/backBtn.png'
+
+export default function Header() {
+  return (
+    <div className='header-WriteReview'>
+        <Link to="/MyPage">
+            <img src={scrBackBtn} alt="뒤로가기" />
+        </Link>
+        <p className='title-header'>교내 식당 IN</p>
+    </div>
+  )
+}
---
+++ src/pages/StoreListIn.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/StoreListIn/Header'
+
+export default function StoreListIn() {
+  return (
+    <div className='BottomNav3-StoreListIn'>
+        <Header />
+    </div>
+  )
+}
