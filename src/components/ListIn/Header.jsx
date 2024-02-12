5fc6d05b3e24e59d9e03083a1a89c14f155aa9fe
sieun1002 <nata20034@naver.com>
2024-02-13 01:01:19 +0900

    (시은) WaitinRank(실시간 웨이팅 랭킹 페이지) BottomNav3과 연결

--- src/components/ListIn/Header.jsx
+++ src/components/ListIn/Header.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 
 import scrBackBtn from "../../images/WriteReview/backBtn.png";
 
-export default function Header() {
+export default function Header(props) {
+  const title = props.title;
   return (
     <div className="header-WriteReview">
       <Link to="/List">
         <img src={scrBackBtn} alt="뒤로가기" />
       </Link>
-      <p className="title-header">교내 식당 IN</p>
+      <p className="title-header">{title}</p>
     </div>
   );
 }
